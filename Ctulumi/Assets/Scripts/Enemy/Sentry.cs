ca4b683ae3b4e96f8c753ea28627dfe5ac6c30c9
XerdesAE <44979681+XerdesAE@users.noreply.github.com>
2020-01-21 16:20:38 +0100

    sentryvision

--- Ctulumi/Assets/Scripts/Enemy/Sentry.cs
+++ Ctulumi/Assets/Scripts/Enemy/Sentry.cs
@@ -1,39 +1,41 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Experimental.Rendering.LWRP;
+
 
 public class Sentry : MonoBehaviour
 {
-    private bool blinking;//si la sentinelle cligne ou non (incompatible avec les sentinelles mobiles)
-    private bool blinked;
-    private bool[] orientations; //liste des différentes orientations de la sentinelle
-    private int watchtime;//temps avant que la sentinelle change d'orientation
-    private float chronometer;
-    private int index;//savoir quelle orientation choisir dans la liste
-    private int blinktime;//temps avant clignage
+    public Light2D observerLight;
+    public FieldOfView field;
 
-    public Sentry(bool blink, bool[] oris, int startingOrientation, int watime=2)
-    {
-        blinking = blink;
-        orientations = oris;
-        watchtime = watime;
-        index = startingOrientation;
-    }
-    public Sentry(bool blink, int soleOrientation)//si on veut faire une sentinelle fixe
-    {
-        blinking = blink;
-        orientations = new bool[3];
-        orientations[soleOrientation] = true;
-        watchtime = 0;
-    }
+    public float viewRadius;
+    public float viewAngle;
+
+    public bool blinking;//si la sentinelle cligne ou non (incompatible avec les sentinelles mobiles)
+    public bool[] orientations; //liste des différentes orientations de la sentinelle
+    public int currentPosition;//position actuelle
+    public int watchtime;//temps avant que la sentinelle change d'orientation
+    public int blinktime;//temps avant clignage
+
+    private bool blinked;
+    private float chronometer;
+
     // Start is called before the first frame update
     void Start()
     {
         chronometer = 0;
-        index = 0;
-        blinktime = 2;
+        currentPosition = 0;
+        viewRadius = 8;
+        viewAngle = 80;
         blinking = false;
-        //orienter la sentinelle dans son orientation par défaut (orientations[])
+        observerLight.pointLightInnerAngle = viewAngle;
+        observerLight.pointLightOuterAngle = viewAngle;
+        observerLight.pointLightInnerRadius = viewRadius;
+        observerLight.pointLightOuterRadius = viewRadius;
+        field.viewRadius = viewRadius;
+        field.viewAngle = viewAngle;
+        field.gameObject.transform.eulerAngles = new Vector3(0,0,((currentPosition - 1) * 45));//hacky math
     }
 
     // Update is called once per frame
@@ -44,12 +46,15 @@ public class Sentry : MonoBehaviour
             chronometer += Time.deltaTime;//update chrono
             if (chronometer > watchtime)//à la fin du chrono
             {
-                chronometer = 0;//reset chrono
-                if (index == 2)//si on dépasse la liste orientations
-                    index = 0;//on revient au début
+                if (currentPosition == 2)//si on dépasse la liste orientations
+                    currentPosition = 0;//on revient au début
                 else
-                    index++;//sinon on passe à la valeur suivante
-                //changer l'orientation en fonction de l'index
+                    currentPosition++;//sinon on passe à la valeur suivante
+                if (orientations[currentPosition])
+                {
+                    field.gameObject.transform.eulerAngles = new Vector3(0, 0, ((currentPosition - 1) * 45));
+                    chronometer = 0;//reset chrono
+                }
             }
         }
         else if(blinking)
