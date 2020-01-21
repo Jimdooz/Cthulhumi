3fb86e9d03f9fe23e4a6bca88b9bd5ce57e6c71f
XerdesAE <44979681+XerdesAE@users.noreply.github.com>
2020-01-21 14:24:43 +0100

    Update Sentry.cs

--- Ctulumi/Assets/Scripts/Enemy/Sentry.cs
+++ Ctulumi/Assets/Scripts/Enemy/Sentry.cs
@@ -4,22 +4,26 @@ using UnityEngine;
 
 public class Sentry : MonoBehaviour
 {
-    private bool blinking;//si la sentinelle cligne ou non
-    private List<int> orientations; //liste des différentes orientations de la sentinelle
+    private bool blinking;//si la sentinelle cligne ou non (incompatible avec les sentinelles mobiles)
+    private bool blinked;
+    private bool[] orientations; //liste des différentes orientations de la sentinelle
     private int watchtime;//temps avant que la sentinelle change d'orientation
     private float chronometer;
     private int index;//savoir quelle orientation choisir dans la liste
+    private int blinktime;//temps avant clignage
 
-    public Sentry(bool blink, List<int> oris, int watime=2)
+    public Sentry(bool blink, bool[] oris, int startingOrientation, int watime=2)
     {
         blinking = blink;
         orientations = oris;
         watchtime = watime;
+        index = startingOrientation;
     }
     public Sentry(bool blink, int soleOrientation)//si on veut faire une sentinelle fixe
     {
         blinking = blink;
-        orientations.push(soleOrientation);
+        orientations = new bool[3];
+        orientations[soleOrientation] = true;
         watchtime = 0;
     }
     // Start is called before the first frame update
@@ -27,7 +31,9 @@ public class Sentry : MonoBehaviour
     {
         chronometer = 0;
         index = 0;
-        //orienter la sentinelle dans son orientation par défaut (orientations[1])
+        blinktime = 2;
+        blinking = false;
+        //orienter la sentinelle dans son orientation par défaut (orientations[])
     }
 
     // Update is called once per frame
@@ -39,11 +45,20 @@ public class Sentry : MonoBehaviour
             if (chronometer > watchtime)//à la fin du chrono
             {
                 chronometer = 0;//reset chrono
-                if (index + 1 > orientations.Count)//si on dépasse la liste orientations
+                if (index == 2)//si on dépasse la liste orientations
                     index = 0;//on revient au début
                 else
                     index++;//sinon on passe à la valeur suivante
-                //changer l'orientation à la valeur de orientations[index]
+                //changer l'orientation en fonction de l'index
+            }
+        }
+        else if(blinking)
+        {
+            chronometer += Time.deltaTime;
+            if (chronometer > blinktime)
+            {
+                blinked = !blinked;
+                chronometer = 0;
             }
         }
         //si le joueur est en vue, le tuer
