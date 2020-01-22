6f19d65cf0435c2f34fdbbfc60e13fc1e8739061
Jimdooz <romain.saclier@gmail.com>
2020-01-22 05:38:16 +0100

    SplashScreen

---
+++ Ctulumi/Assets/Scripts/SplashScreen/SplashScreen.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SplashScreen : MonoBehaviour
+{
+    public string LevelToLoad;
+    private bool continueScene = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            if (!continueScene)
+            {
+                continueScene = true;
+                GetComponent<Animator>().SetBool("continue", true);
+            }
+        }
+    }
+
+    public void LoadLevel()
+    {
+        SceneManager.LoadScene(LevelToLoad);
+    }
+}
