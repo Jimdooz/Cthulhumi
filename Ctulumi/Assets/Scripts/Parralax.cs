e5b0a5901b70fda906a63e1ed8eba98ca56e3ff4
Jimdooz <romain.saclier@gmail.com>
2020-01-22 04:19:14 +0100

    ezz

---
+++ Ctulumi/Assets/Scripts/Parralax.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parralax : MonoBehaviour
+{
+    private float length, startpos;
+    public GameObject cam;
+    public float ParralaxEffect;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startpos = transform.position.x;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float dist = (cam.transform.position.x * ParralaxEffect);
+
+        transform.position = new Vector3(startpos + dist, transform.position.y, transform.position.z);
+    }
+}
